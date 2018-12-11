729bfc2cbd7bf566c18bf516e4bccde18e472da6
milw0rm55 <sebastiantace@gmail.com>
2018-12-11 14:30:09 +0100

    chat terminado

--- src/Chat/Emisor.java
+++ src/Chat/Emisor.java
@@ -1,5 +1,31 @@
 package Chat;
 
-public class Emisor {
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+public class Emisor {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			InetSocketAddress addr2 = new InetSocketAddress("localhost", 5556);
+			DatagramSocket datagramSocket1 = new DatagramSocket(addr2);
+			DatagramSocket datagramSocket = new DatagramSocket();
+			System.out.println("Elige Nombre");
+			String nombre = sc.nextLine();
+			boolean bucle = true;
+			while (bucle == true){
+				byte[] respuesta = new byte[1024];
+				String mensaje = nombre + " : "+ sc.nextLine();
+				InetAddress addr = InetAddress.getByName("localhost");
+				DatagramPacket datagrama2 = new DatagramPacket(respuesta, 1024);
+				DatagramPacket datagrama = new DatagramPacket(mensaje.getBytes(), mensaje.getBytes().length, addr, 5555);
+				datagramSocket.send(datagrama);
+				datagramSocket1.receive(datagrama2);
+				System.out.println(new String(respuesta));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/Chat/Receptor.java
+++ src/Chat/Receptor.java
@@ -1,5 +1,34 @@
 package Chat;
 
-public class Receptor {
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+public class Receptor {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+		//	System.out.println("Creando el socket datagram");
+			InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
+			DatagramSocket datagramSocket = new DatagramSocket(addr);
+			System.out.println("Elige Nombre");
+			String name = sc.nextLine();
+			boolean bucle = true;
+			while(bucle == true) {
+				byte[] mensaje = new byte[1024];
+				DatagramPacket datagrama1 = new DatagramPacket(mensaje, 1024);
+				datagramSocket.receive(datagrama1);
+				System.out.println(new String(mensaje));
+				System.out.println("Respuesta");
+				String respuesta = name + " : "+ sc.nextLine();;
+				InetAddress addr2 = InetAddress.getByName("localhost");
+				DatagramPacket datagrama2 = new DatagramPacket(respuesta.getBytes(), respuesta.getBytes().length, addr2, 5556);
+				datagramSocket.send(datagrama2);
+				System.out.println("Mensaje enviado");
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
