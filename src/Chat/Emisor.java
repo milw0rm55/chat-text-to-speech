c1dd64623a50fee4d1980b12f66c7a94124d1d6d
milw0rm55 <sebastiantace@gmail.com>
2018-12-12 01:22:18 +0100

    speech to text

--- src/Chat/Emisor.java
+++ src/Chat/Emisor.java
@@ -4,9 +4,12 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
+
 public class Emisor {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		voce.SpeechInterface.synthesize("hello World");
+		
 		try {
 			InetSocketAddress addr2 = new InetSocketAddress("localhost", 5556);
 			DatagramSocket datagramSocket1 = new DatagramSocket(addr2);
@@ -14,15 +17,19 @@ public class Emisor {
 			System.out.println("Elige Nombre");
 			String nombre = sc.nextLine();
 			boolean bucle = true;
+			voce.SpeechInterface.init("../../../lib", true, false, "", "");
 			while (bucle == true){
 				byte[] respuesta = new byte[1024];
-				String mensaje = nombre + " : "+ sc.nextLine();
+				String mensaje = nombre + " says "+" : "+ sc.nextLine();
 				InetAddress addr = InetAddress.getByName("localhost");
 				DatagramPacket datagrama2 = new DatagramPacket(respuesta, 1024);
 				DatagramPacket datagrama = new DatagramPacket(mensaje.getBytes(), mensaje.getBytes().length, addr, 5555);
 				datagramSocket.send(datagrama);
 				datagramSocket1.receive(datagrama2);
 				System.out.println(new String(respuesta));
+				voce.SpeechInterface.synthesize(new String(respuesta));
+		        try { Thread.sleep(100000); } catch(Exception ex) {}
+		        voce.SpeechInterface.destroy();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/Chat/Receptor.java
+++ src/Chat/Receptor.java
@@ -14,17 +14,21 @@ public class Receptor {
 			System.out.println("Elige Nombre");
 			String name = sc.nextLine();
 			boolean bucle = true;
+			voce.SpeechInterface.init("../../../lib", true, false, "", "");
 			while(bucle == true) {
 				byte[] mensaje = new byte[1024];
 				DatagramPacket datagrama1 = new DatagramPacket(mensaje, 1024);
 				datagramSocket.receive(datagrama1);
 				System.out.println(new String(mensaje));
 				System.out.println("Respuesta");
-				String respuesta = name + " : "+ sc.nextLine();;
+				String respuesta = name +" says " + " : "+ sc.nextLine();;
 				InetAddress addr2 = InetAddress.getByName("localhost");
 				DatagramPacket datagrama2 = new DatagramPacket(respuesta.getBytes(), respuesta.getBytes().length, addr2, 5556);
 				datagramSocket.send(datagrama2);
 				System.out.println("Mensaje enviado");
+				voce.SpeechInterface.synthesize(new String(mensaje));
+		        try { Thread.sleep(100000); } catch(Exception ex) {}
+		        voce.SpeechInterface.destroy();
 			}
 			
 		} catch (IOException e) {
